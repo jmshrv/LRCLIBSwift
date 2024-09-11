47dd22fc08321dc8d3e6112f5f6b64716a96a6f3
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-09-11 17:58:43 +0100

    Add platforms/struct

--- Sources/LRCLIBSwift/LRCLIBSwift.swift
+++ Sources/LRCLIBSwift/LRCLIBSwift.swift
@@ -1,2 +1,6 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+/// A LRCLIB client. All methods are accessed statically - LRCLIB doesn't need any state to be kept.
+struct LrcLib {
+    private static let userAgent = "LRCLIBSwift v1.0.0 (https://github.com/jmshrv/LRCLIBSwift)"
+}
